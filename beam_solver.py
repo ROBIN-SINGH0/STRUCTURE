4b76660861a1fcb961407fdb186e48719aab7cca
ROBIN_SINGH <parmarrobin29@gmail.com>
2026-01-31 21:13:06 +0530

    Update beam_solver.py

--- beam_solver.py
+++ beam_solver.py
@@ -4,42 +4,19 @@ import numpy as np
 # Beam Element
 # -----------------------------
 class BeamElement:
-    def __init__(self, E, I, L):
-        self.E = E
-        self.I = I
+    def __init__(self, L):
         self.L = L
 
     def stiffness(self):
         L = self.L
-        return (self.E*self.I/L**3) * np.array([
+        return (1 / L**3) * np.array([
             [12, 6*L, -12, 6*L],
             [6*L, 4*L**2, -6*L, 2*L**2],
             [-12, -6*L, 12, -6*L],
             [6*L, 2*L**2, -6*L, 4*L**2]
         ])
 
-    # constant UDL
-    def udl_eq(self, w):
-        L = self.L
-        return np.array([
-            -w*L/2,
-            -w*L**2/12,
-            -w*L/2,
-             w*L**2/12
-        ])
-
-    # triangular / trapezoidal load
-    def trap_udl_eq(self, w0, wL):
-        L = self.L
-        return np.array([
-            -(7*w0+3*wL)*L/20,
-            -(w0+2*wL)*L**2/60,
-            -(3*w0+7*wL)*L/20,
-             (2*w0+wL)*L**2/60
-        ])
-
-    # point load inside element
-    def point_inside(self, a, P):
+    def point_load_eq(self, a, P):
         L = self.L
         b = L - a
         return np.array([
@@ -54,98 +31,85 @@ class BeamElement:
 # Beam Model
 # -----------------------------
 class Beam:
-    def __init__(self, length, mesh):
+    def __init__(self, length):
         self.length = length
-        self.mesh = mesh
-        self.dx = length / mesh
-        self.nodes = [i*self.dx for i in range(mesh+1)]
+        self.supports = {}        # x : type
+        self.point_loads = []     # (x, P)
 
-        self.elements = []
-        self.loads = []
-        self.supports = {}
-
-    def add_element(self, E, I):
-        self.elements.append(BeamElement(E, I, self.dx))
-
-    # -------- loads ----------
-    def add_point_load(self, x, P):
-        self.loads.append(("P", x, P))
-
-    def add_udl(self, x1, x2, w):
-        self.loads.append(("UDL", x1, x2, w))
-
-    def add_trapezoidal_load(self, x1, x2, w0, wL):
-        self.loads.append(("TRAP", x1, x2, w0, wL))
-
-    def add_moment(self, x, M):
-        self.loads.append(("M", x, M))
-
-    # -------- supports ----------
     def add_support(self, x, stype):
         self.supports[x] = stype.lower()
 
-    # -------- solver ----------
+    def add_point_load(self, x, P):
+        self.point_loads.append((x, P))
+
     def solve(self):
-        n = len(self.nodes)
-        dof = 2*n
+        # -------- create nodes ----------
+        nodes = set([0, self.length])
+        for x in self.supports:
+            nodes.add(x)
+        for x, _ in self.point_loads:
+            nodes.add(x)
+
+        nodes = sorted(nodes)
+        n = len(nodes)
+        dof = 2 * n
+
+        # -------- create elements ----------
+        elements = []
+        for i in range(n - 1):
+            L = nodes[i+1] - nodes[i]
+            elements.append(BeamElement(L))
+
         K = np.zeros((dof, dof))
         F = np.zeros(dof)
 
-        # assemble stiffness
-        for i, el in enumerate(self.elements):
+        # -------- assemble stiffness ----------
+        for i, el in enumerate(elements):
             k = el.stiffness()
-            idx = [2*i,2*i+1,2*i+2,2*i+3]
+            idx = [2*i, 2*i+1, 2*i+2, 2*i+3]
             for a in range(4):
                 for b in range(4):
-                    K[idx[a], idx[b]] += k[a,b]
+                    K[idx[a], idx[b]] += k[a, b]
 
-        # apply loads
-        for L in self.loads:
-
-            if L[0] == "P":
-                x, P = L[1], L[2]
-                i = int(x/self.dx)
-                a = x - i*self.dx
-                fe = self.elements[i].point_inside(a, P)
-                idx = [2*i,2*i+1,2*i+2,2*i+3]
+        # -------- apply point loads ----------
+        for x, P in self.point_loads:
+            i = nodes.index(x)
+            if i < n - 1:
+                fe = elements[i].point_load_eq(0, P)
+                idx = [2*i, 2*i+1, 2*i+2, 2*i+3]
                 for j in range(4):
                     F[idx[j]] += fe[j]
+            else:
+                F[2*i] += P
 
-            elif L[0] == "UDL":
-                x1,x2,w = L[1:]
-                i1,i2 = int(x1/self.dx), int(x2/self.dx)
-                for i in range(i1,i2):
-                    fe = self.elements[i].udl_eq(w)
-                    idx = [2*i,2*i+1,2*i+2,2*i+3]
-                    for j in range(4):
-                        F[idx[j]] += fe[j]
-
-            elif L[0] == "TRAP":
-                x1,x2,w0,wL = L[1:]
-                i1,i2 = int(x1/self.dx), int(x2/self.dx)
-                for i in range(i1,i2):
-                    fe = self.elements[i].trap_udl_eq(w0,wL)
-                    idx = [2*i,2*i+1,2*i+2,2*i+3]
-                    for j in range(4):
-                        F[idx[j]] += fe[j]
-
-            elif L[0] == "M":
-                x,M = L[1],L[2]
-                n = int(x/self.dx)
-                F[2*n+1] += M
-
-        # supports → DOF fixing
+        # -------- apply supports ----------
         fixed = []
-        for x,st in self.supports.items():
-            n = int(x/self.dx)
+        for x, st in self.supports.items():
+            i = nodes.index(x)
             if st == "fixed":
-                fixed += [2*n,2*n+1]
-            elif st in ["hinge","roller"]:
-                fixed += [2*n]
+                fixed += [2*i, 2*i+1]
+            elif st in ["hinge", "roller"]:
+                fixed += [2*i]
 
         free = list(set(range(dof)) - set(fixed))
 
+        # -------- solve ----------
         D = np.zeros(dof)
-        D[free] = np.linalg.solve(K[np.ix_(free,free)], F[free])
-        R = K@D - F
-        return D, R 
+        D[free] = np.linalg.solve(K[np.ix_(free, free)], F[free])
+
+        # -------- reactions ----------
+        R = K @ D - F
+
+        # -------- bending moment ----------
+        BM = []
+        for i, el in enumerate(elements):
+            k = el.stiffness()
+            idx = [2*i, 2*i+1, 2*i+2, 2*i+3]
+            d_local = D[idx]
+            f_local = k @ d_local
+
+            M_left = f_local[1]
+            M_right = -f_local[3]
+            BM.append((nodes[i], nodes[i+1], M_left, M_right))
+
+        return nodes, R, BM
