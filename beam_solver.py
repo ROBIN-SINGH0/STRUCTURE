143942d50f620d3d71ec8e9f57e8ae9e976a20c8
ROBIN_SINGH <parmarrobin29@gmail.com>
2026-01-31 22:40:45 +0530

    Update beam_solver.py

--- beam_solver.py
+++ beam_solver.py
@@ -63,7 +63,7 @@ class Beam:
         self.uvls.append((x1, x2, w1, w2))
 
     def solve(self):
-        # -------- nodes ----------
+        # -------- Nodes ----------
         nodes = set([0, self.length])
         for x in self.supports: nodes.add(x)
         for x in self.internal_hinges: nodes.add(x)
@@ -75,7 +75,7 @@ class Beam:
         n = len(nodes)
         dof = 2*n
 
-        # -------- elements ----------
+        # -------- Elements ----------
         elements = []
         for i in range(n-1):
             elements.append(BeamElement(nodes[i+1]-nodes[i]))
@@ -83,7 +83,7 @@ class Beam:
         K = np.zeros((dof,dof))
         F = np.zeros(dof)
 
-        # -------- stiffness ----------
+        # -------- Stiffness ----------
         for i,el in enumerate(elements):
             k = el.stiffness()
             idx = [2*i,2*i+1,2*i+2,2*i+3]
@@ -91,15 +91,15 @@ class Beam:
                 for b in range(4):
                     K[idx[a],idx[b]] += k[a,b]
 
-        # -------- internal hinge ----------
+        # -------- Internal hinge ----------
         for x in self.internal_hinges:
             i = nodes.index(x)
-            r = 2*i+1
+            r = 2*i + 1
             K[r,:] = 0
             K[:,r] = 0
             K[r,r] = 1e-9
 
-        # -------- point loads ----------
+        # -------- Point loads ----------
         for x,P in self.point_loads:
             i = nodes.index(x)
             F[2*i] += P
@@ -108,7 +108,7 @@ class Beam:
         for x1,x2,w in self.udls:
             for i in range(n-1):
                 a,b = nodes[i], nodes[i+1]
-                overlap = max(0, min(b,x2)-max(a,x1))
+                overlap = max(0, min(b,x2) - max(a,x1))
                 if overlap > 0:
                     fe = elements[i].udl_eq(w * overlap/(b-a))
                     idx=[2*i,2*i+1,2*i+2,2*i+3]
@@ -118,14 +118,14 @@ class Beam:
         for x1,x2,w1,w2 in self.uvls:
             for i in range(n-1):
                 a,b = nodes[i], nodes[i+1]
-                overlap = max(0, min(b,x2)-max(a,x1))
+                overlap = max(0, min(b,x2) - max(a,x1))
                 if overlap > 0:
                     r = overlap/(b-a)
                     fe = elements[i].uvl_eq(w1*r, w2*r)
                     idx=[2*i,2*i+1,2*i+2,2*i+3]
                     for j in range(4): F[idx[j]] += fe[j]
 
-        # -------- supports ----------
+        # -------- Supports ----------
         fixed=[]
         for x,st in self.supports.items():
             i=nodes.index(x)
@@ -134,34 +134,36 @@ class Beam:
 
         free=list(set(range(dof))-set(fixed))
 
-        # -------- solve ----------
+        # -------- Solve ----------
         D=np.zeros(dof)
         D[free]=np.linalg.solve(K[np.ix_(free,free)],F[free])
 
-        # -------- reactions ----------
+        # -------- Reactions ----------
         R=K@D-F
         reactions={}
         for x in self.supports:
             i=nodes.index(x)
             reactions[x]=round(R[2*i],3)
 
-        # -------- SFD + BMD ----------
-        results=[]
+        # -------- BOOK STYLE SF & BM ----------
+        shear={}
+        moment={}
+
         for i,el in enumerate(elements):
             f = el.stiffness() @ D[[2*i,2*i+1,2*i+2,2*i+3]]
+            V_L = -f[0]
+            V_R =  f[2]
+            M_L =  f[1]
+            M_R = -f[3]
 
-            V_left  = round(-f[0],3)
-            V_right = round(f[2],3)
-            M_left  = round(f[1],3)
-            M_right = round(-f[3],3)
+            shear[nodes[i]] = round(V_L,3)
+            moment[nodes[i]] = round(M_L,3)
 
-            if nodes[i] in self.internal_hinges: M_left = 0
-            if nodes[i+1] in self.internal_hinges: M_right = 0
+            if i == len(elements)-1:
+                shear[nodes[i+1]] = round(V_R,3)
+                moment[nodes[i+1]] = round(M_R,3)
 
-            results.append((
-                nodes[i], nodes[i+1],
-                V_left, V_right,
-                M_left, M_right
-            ))
+        for x in self.internal_hinges:
+            moment[x] = 0
 
-        return reactions, results
+        return reactions, shear, moment
