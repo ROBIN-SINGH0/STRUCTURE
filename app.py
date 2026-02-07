f3184694a6d7cde336ae88993a9c1610d9fa2ca4
ROBIN_SINGH <parmarrobin29@gmail.com>
2026-02-07 20:47:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def generate_labels(n):
 # -----------------------------
 
 # ✅ KEEP ONLY THIS
-def sf_at_x_right(xp, reactions, point_loads, udls, uvls, L):
+def sf_at_x(xp, reactions, point_loads, udls, uvls, L):
     Vx = 0.0
     tol = 1e-6
 
