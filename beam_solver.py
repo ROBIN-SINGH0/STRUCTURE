33187c175f9d204b3daf9b807af029c99a37ff65
ROBIN_SINGH <parmarrobin29@gmail.com>
2026-02-07 20:40:40 +0530

    Update beam_solver.py

--- beam_solver.py
+++ beam_solver.py
@@ -1,87 +1,147 @@
 import numpy as np
 
-# =================================
+# -----------------------------
 # Beam Element (Euler–Bernoulli)
-# =================================
+# -----------------------------
 class BeamElement:
-    def __init__(self, L, E, I):
+    def __init__(self, L):
         self.L = L
-        self.E = E
-        self.I = I
 
     def stiffness(self):
         L = self.L
-        EI = self.E * self.I
-        return EI / L**3 * np.array([
+        return (1 / L**3) * np.array([
             [12,   6*L,  -12,   6*L],
             [6*L, 4*L**2, -6*L, 2*L**2],
             [-12, -6*L,   12,  -6*L],
             [6*L, 2*L**2, -6*L, 4*L**2]
         ])
 
+    def udl_eq(self, w):
+        L = self.L
+        return np.array([
+            -w*L/2,
+            -w*L**2/12,
+            -w*L/2,
+             w*L**2/12
+        ])
 
-# =================================
-# FEM Solver (Reactions Only)
-# =================================
+    def uvl_eq(self, w1, w2):
+        L = self.L
+        return np.array([
+            -(7*w1 + 3*w2)*L/20,
+            -(w1 + 2*w2)*L**2/60,
+            -(3*w1 + 7*w2)*L/20,
+             (2*w1 + w2)*L**2/60
+        ])
+
+
+# -----------------------------
+# Beam Model
+# -----------------------------
 class Beam:
-    def __init__(self, length, n_elem=10, E=2e11, I=8e-6):
-        self.L = length
-        self.n = n_elem
-        self.E = E
-        self.I = I
-        self.le = length / n_elem
-
-        self.loads = []
+    def __init__(self, length):
+        self.length = length
         self.supports = {}
-
-        self.ndof = 2 * (n_elem + 1)
-        self.K = np.zeros((self.ndof, self.ndof))
-        self.F = np.zeros(self.ndof)
+        self.internal_hinges = set()
+        self.point_loads = []
+        self.udls = []
+        self.uvls = []
 
     def add_support(self, x, stype):
-        node = int(x / self.le)
-        self.supports[node] = stype
+        if stype == "internal_hinge":
+            self.internal_hinges.add(x)
+        else:
+            self.supports[x] = stype
 
-    def add_point_load(self, P, x):
-        node = int(x / self.le)
-        self.loads.append(("point", P, x))
-        self.F[2 * node] -= P
+    def add_point_load(self, x, P):
+        self.point_loads.append((x, P))
 
-    def assemble(self):
-        for i in range(self.n):
-            elem = BeamElement(self.le, self.E, self.I)
-            k = elem.stiffness()
+    def add_udl(self, x1, x2, w):
+        self.udls.append((x1, x2, w))
 
-            dof = [2*i, 2*i+1, 2*i+2, 2*i+3]
-            for a in range(4):
-                for b in range(4):
-                    self.K[dof[a], dof[b]] += k[a, b]
+    def add_uvl(self, x1, x2, w1, w2):
+        self.uvls.append((x1, x2, w1, w2))
 
-    def solve_reactions(self):
-        self.assemble()
+    def solve(self, npts=20):
 
+        # ---------- Nodes ----------
+        nodes = {0, self.length}
+        for x in self.supports:
+            nodes.add(x)
+        for x in self.internal_hinges:
+            nodes.add(x)
+        for x, _ in self.point_loads:
+            nodes.add(x)
+        for x1, x2, _ in self.udls:
+            nodes.update([x1, x2])
+        for x1, x2, _, _ in self.uvls:
+            nodes.update([x1, x2])
+
+        nodes = sorted(nodes)
+        n = len(nodes)
+        dof = 2 * n
+
+        # ---------- Elements ----------
+        elements = [BeamElement(nodes[i+1] - nodes[i]) for i in range(n - 1)]
+
+        K = np.zeros((dof, dof))
+        F = np.zeros(dof)
+
+        # ---------- Stiffness ----------
+        for i, el in enumerate(elements):
+            k = el.stiffness()
+            idx = [2*i, 2*i+1, 2*i+2, 2*i+3]
+            K[np.ix_(idx, idx)] += k
+
+        # ---------- Internal hinges ----------
+        for x in self.internal_hinges:
+            i = nodes.index(x)
+            r = 2*i + 1
+            K[r, :] = 0
+            K[:, r] = 0
+            K[r, r] = 1e-9
+
+        # ---------- Loads ----------
+        for x, P in self.point_loads:
+            F[2 * nodes.index(x)] += P
+
+        for x1, x2, w in self.udls:
+            for i in range(n - 1):
+                a, b = nodes[i], nodes[i + 1]
+                ov = max(0, min(b, x2) - max(a, x1))
+                if ov > 0:
+                    fe = elements[i].udl_eq(w * ov / (b - a))
+                    F[2*i:2*i+4] += fe
+
+        for x1, x2, w1, w2 in self.uvls:
+            for i in range(n - 1):
+                a, b = nodes[i], nodes[i + 1]
+                ov = max(0, min(b, x2) - max(a, x1))
+                if ov > 0:
+                    r = ov / (b - a)
+                    fe = elements[i].uvl_eq(w1 * r, w2 * r)
+                    F[2*i:2*i+4] += fe
+
+        # ---------- Supports ----------
         fixed = []
-        for node, stype in self.supports.items():
-            if stype == "fixed":
-                fixed += [2*node, 2*node+1]
-            else:  # pinned / roller
-                fixed += [2*node]
+        for x, st in self.supports.items():
+            i = nodes.index(x)
+            if st == "fixed":
+                fixed += [2*i, 2*i+1]
+            elif st in ["hinge", "roller"]:
+                fixed += [2*i]
 
-        all_dof = np.arange(self.ndof)
-        free = np.setdiff1d(all_dof, fixed)
+        free = list(set(range(dof)) - set(fixed))
 
-        Kff = self.K[np.ix_(free, free)]
-        Ff = self.F[free]
+        # ---------- Solve ----------
+        D = np.zeros(dof)
+        D[free] = np.linalg.solve(K[np.ix_(free, free)], F[free])
 
-        Uf = np.linalg.solve(Kff, Ff)
+        # ---------- Reactions ----------
+        R = K @ D - F
+        reactions = {x: R[2*nodes.index(x)] for x in self.supports}
 
-        U = np.zeros(self.ndof)
-        U[free] = Uf
+        return {
+            "reactions": reactions,
+        }
 
-        R = self.K @ U - self.F
-
-        reactions = {}
-        for node in self.supports:
-            reactions[node * self.le] = R[2 * node]
-
-        return reactions
